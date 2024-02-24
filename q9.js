d0795e4d2f32f246c356b2430ff573898fd583ba
TALHA HAROON <152915208+talha765haroon@users.noreply.github.com>
2024-02-25 03:24:22 +0500

    Add files via upload

---
+++ q9.js
@@ -0,0 +1,6 @@
+"use strict";
+/* Ques # 09:
+Favourite number: store your favourite number in a variable, Then using that variable.
+Then, using that variable, create a message that reveals your favourite number.print that message.*/
+let num = 45;
+console.log(`My favourite number is ${num}`);
---
+++ q9.ts
@@ -0,0 +1,7 @@
+/* Ques # 09:
+Favourite number: store your favourite number in a variable, Then using that variable.
+Then, using that variable, create a message that reveals your favourite number.print that message.*/
+
+let num :number = 45;
+
+console.log(`My favourite number is ${num}`)
